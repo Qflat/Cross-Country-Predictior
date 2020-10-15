11cc592ddf9d1cc02df8500581d8215e9e0f9235
Jaden Costa <31477326+Qflat@users.noreply.github.com>
2020-10-15 19:05:52 -0400

    Updated algorithm
    Still need to fix one odd setting in finding the last date ran for one athlete. Specifically by using the checking date if statement (line 97)

--- final_project.py
+++ final_project.py
@@ -8,7 +8,7 @@ class Webpage:
     def __init__(self,master):
         self.master=master
         master.title('Web Results')
-        master.label=Label(text='Please insert the seasons best list you\'d like to evaluate: ')
+        master.label=Label(text='Please insert the hypothetical meet list you\'d like to evaluate: ')
         master.label.grid(row=0)
         master.url_box=Entry(master)
         master.url_box.grid(row=1)
@@ -39,10 +39,30 @@ def text(line):
     final_val=new_val[0]
     return final_val
 
+def timed(result):
+    splitted=result.split('>')
+    val=splitted[4]
+    s=splitted[4].split('<')
+    time=s[0]
+    nums=time.split(':')
+    try:
+        minutes=float(str((nums[0])))
+        seconds=float(str((nums[1])))
+        while minutes>0.0:
+            minutes-=1.0
+            seconds+=60.0
+        return seconds
+    except ValueError:
+        # No Final Result
+        return 9999
+
+# Defining a few more variables that'll be important later
 today=datetime.today()
 year=str(today.year)
 a=lines(url,'a')
 line_count=0
+
+# Run through each athlete to gather Data set
 for line in a:
     line=str(line)
     if line_count>=58:  # Team Information Begins on Line 58 in Hypothetical Meet Page
@@ -55,25 +75,34 @@ for line in a:
             athlete_page=lines("https://www.athletic.net/CrossCountry/"+line[12:37],'td')
             num=0
             dates=[]
+            times=[]
             for a_line in athlete_page:
+                a_line=str(a_line)
                 if num>0:
                     if num<=3:
                         num+=1
                     else:
-                        if 'meet' in a_line:
+                        if 'results' in a_line:
                             pass
                         elif 'result' in a_line:
-                            time=text(a_line)
+                            time=timed(a_line)
+                            times.append(time)
                         elif 'width' in a_line:
-                            print("running")
                             date=text(a_line)
-                            dates.append(date)
-                        elif 'appC' in a_line:
-                            break
+                            if date!='':
+                                dates.append(date)    
                 elif year in a_line:
                     num+=1
+            for i in range(0,len(dates)):
+                if dates[i] < dates[i+1]:
+                    pass
+                else:
+                    last_date_ran=dates[i]
+                    break
+            for i in range(0,len(dates)):
+                if last_date_ran==dates[i]:
+                    season_best=min(times[:i])
+                    break
         elif 'result' in line:  # Time Information
             pass
-        #print(line)
-        #for a_line in athlete_page:
     line_count+=1
