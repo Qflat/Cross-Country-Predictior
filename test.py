fa295de5707d9ebfe2254cc8525600608ee914d7
Jaden Costa <31477326+Qflat@users.noreply.github.com>
2020-11-02 13:05:10 -0500

    Update Array

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 from dataset import vals as data
-import pandas as pd
+from pandas import DataFrame as DataFrame
 import matplotlib.pyplot as plt
-df=pd.DataFrame(data, columns=['Name','School','X-Coordinate','Y-Coordinate','Season Best','SB Date'])
-X=[[],[]]
+import numpy as np
+df=DataFrame(data, columns=['Name','School','X-Coordinate','Y-Coordinate','Season Best','SB Date'])
+x=[]
+y=[]
 for val in df['X-Coordinate']:
-    X[0].append(val)
+    x.append(val)
 for val in df['Y-Coordinate']:
-    X[1].append(val)
+    y.append(val)
+
+X={'x':x,'y':y}
+df_vals=DataFrame(X,columns=['x','y'])
 
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
@@ -15,7 +20,7 @@ from sklearn.preprocessing import StandardScaler
 wcss=[]
 for i in range(1,11):
     kmeans=KMeans(n_clusters=i,init='k-means++',max_iter=300,n_init=10,random_state=0,copy_x=True)
-    kmeans.fit(X)
+    kmeans.fit(df_vals)
     wcss.append(kmeans.inertia_)
 plt.plot(range(1,11), wcss)
 plt.title('Elbow Method')
